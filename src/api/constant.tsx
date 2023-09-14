fbb82830856ced844f6b642906164b80b2420dee
your_username <your_email@gmail.com>
2023-09-14 23:14:53 +0530

    API setup is done

--- src/api/constant.tsx
+++ src/api/constant.tsx
@@ -9,7 +9,16 @@ export const AUTH = {
 export const ALBUM = {
   CREATE_ALBUM_COVER: AI_API + '/sdapi/v1/txt2img',
 };
+
 export const GENERATE_TRACK = {
   CREATE_TEXT_SONG: API + '/api/Spotify/createTextSong',
   REQUEST_TEXT_SONGS: API + '/api/Spotify/requestTextSongs',
+};
+
+export const REELS = {
+  GET_ALL_REELS: API + '/api/tiktok/requestAllVideos',
+  ADD_VIDEO_ON_REEL: API + '/api/tiktok/addVideo',
+  LIKE_REEL: API + '/api/tiktok/likevideo',
+  SAVE_REEL: API + '/api/tiktok/saveVideo',
+  GET_ALL_SAVE_REEL: API + '/api/tiktok/requestSavedVideos',
 };
---
+++ src/api/reels.tsx
@@ -0,0 +1,86 @@
+import {REELS} from './constant';
+
+const addReelVideo = async (body: any) => {
+  const data = {
+    id: 'MjrK0Yx7O2UlkLqU',
+    link: 'example22.com',
+  };
+  return await fetch(REELS.ADD_VIDEO_ON_REEL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then(response => response.json())
+    .then(value => {
+      return value;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+async function likeVideo() {
+  const data = {
+    id: '5',
+    videoID: 'GyVFDYVL8uFy0fqi',
+    ownerID: '4',
+  };
+  fetch(REELS.LIKE_REEL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(async data => {
+      console.log(await data.json());
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+async function saveVideo() {
+  const data = {
+    id: 'MjrK0Yx7O2UlkLqU',
+    videoID: 'GyVFDYVL8uFy0fqi',
+    ownerID: '4',
+    link: 'example.com',
+  };
+  fetch(REELS.SAVE_REEL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(async data => {
+      console.log(await data.json());
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+async function getSavedVideos() {
+  const data = {
+    id: 'MjrK0Yx7O2UlkLqU',
+  };
+  fetch(REELS.GET_ALL_SAVE_REEL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(async data => {
+      console.log(await data.json());
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+export {addReelVideo, likeVideo, saveVideo, getSavedVideos};
